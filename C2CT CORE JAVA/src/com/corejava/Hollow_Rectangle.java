c085ea0cf76db65a6409a180c48776cf1d1ad085
Skummu <shaikhumair5469@gmail.com>
2022-03-28 11:24:55 +0530

    Hollow_Rectangle

---
+++ C2CT CORE JAVA/src/com/corejava/Hollow_Rectangle.java
@@ -0,0 +1,26 @@
+package com.corejava;
+
+public class Hollow_Rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=4;
+		int m=5;
+		
+		for(int i=1; i<=4; i++) {
+			for(int j=1; j<=5; j++) {
+				if(i==1 || j==1 || i==n || j==m) {
+					System.out.print("*"+" ");
+				}else {
+					System.out.print("  ");
+				}
+				
+			
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
