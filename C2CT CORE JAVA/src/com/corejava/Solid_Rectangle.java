b497765b6c67591324365194856756750fd411fc
Skummu <shaikhumair5469@gmail.com>
2022-03-28 10:49:16 +0530

    Solid Rectangle

---
+++ C2CT CORE JAVA/src/com/corejava/Solid_Rectangle.java
@@ -0,0 +1,22 @@
+package com.corejava;
+
+public class Solid_Rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int n = 4;
+			int m = 5;
+			//outer Loop
+			for(int i=1; i<=n; i++) {
+				//Inner Loop
+				for(int j=1; j<=m; j++) {
+					System.out.print("*"+" ");
+				}
+				System.out.println();
+			}
+
+
+	}
+
+}
