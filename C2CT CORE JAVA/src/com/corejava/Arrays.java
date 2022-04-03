d579ba549956ee94a64b1e26002d789327b4b079
Skummu <shaikhumair5469@gmail.com>
2022-04-03 07:44:40 +0530

    TwoDArrays

--- C2CT CORE JAVA/src/com/corejava/Arrays.java
+++ C2CT CORE JAVA/src/com/corejava/Arrays.java
@@ -1,29 +1,24 @@
 package com.corejava;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Arrays {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Q: Take an Array as an Input from the user.Search for a given number X and print the index at which it Occurs.
-		
 		Scanner sc = new Scanner(System.in);
-		int size = sc.nextInt();
-		int numbers[] = new int[size];
-		//Input
-		for(int i=1; i<size; i++) {
-			numbers[i]=sc.nextInt();
-		}
-		//Output
-		for(int i=1; i<numbers.length; i++) {
-			int x = sc.nextInt();
-			if(numbers[i]==x) {
-				System.out.println("x is found at Index: "+ i);
-			}
+		int  size = sc.nextInt();
+		
+		String names[] = new String[size];
+		
+		for(int i = 0; i<size; i++) {
+			names[i] = sc.next();
 			
 		}
 		
+		for(int i=0; i<names.length; i++) {
+			System.out.println("names " + (i+1) + " is " + names[i] );
+		}
 	}
 
 }
